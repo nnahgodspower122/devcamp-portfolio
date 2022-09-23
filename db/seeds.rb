b2f7be5119d5b2830be6729e4f334301a8dcb672
nnahgodspower122 <nnahgodspower123@gmail.com>
2022-09-23 17:33:47 -0400

    My portfolio changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+    Blog.create do |blog| 
+        title = "My Blog post #{blog}",
+        body = "My new blog post is here"
+    end
+end
+
+5.times do 
+    Skill.create do |skill| 
+        title = "Rails #{skill}",
+        percent_utilized = 15
+    end
+end
+
+9.times do 
+    Portfolio.create do |portfolio_item| 
+        title = "Portfolio title: #{portfolio_item}",
+        subtitle = "my great service",
+        body = "My portfolio details",
+        main_image = "http://placehold.it/600x200",
+        thumb_image = "http://placehold.it/350x200"
+    end
+end
