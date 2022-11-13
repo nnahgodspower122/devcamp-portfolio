db5555555ef76f2524edb3d7cc96d8a9550896d7
nnahgodspower122 <nnahgodspower123@gmail.com>
2022-11-13 13:38:04 -0500

    changes made to view branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
     include SetSource
     include CurrentUserConcern
     include DefaultPageContent
+
+    before_action :set_copyright
+
+    def set_copyright
+        @copyright = DevcampViewTool::Render.copyright 'Jordan Hudgens', 'All rights reserved'
+    end
+
+end
+
+module DevcampViewTool
+    class Render
+        def self.copyright name, msg
+            "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+        end
+    end
 end
